231b37f4ef6929a54dcd112a5e8ab24254769b4f
PratikPatidar <90244554+PratikPatidar@users.noreply.github.com>
2022-02-10 16:47:11 +0530

    Add files via upload

---
+++ resume.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class resume
+{
+    public static void about()
+    {
+        System.out.println("******************************************RESUME******************************************");
+        System.out.println("");
+        System.out.println("Name: Pratik Patidar");
+        System.out.println("Father Name: Omprakash Patidar");
+        System.out.println("Contact No: 8269647990");
+        System.out.println("Gender:  Male");
+        System.out.println("Date of Birth: 15-08-2002");
+        System.out.println("Religion: Hindu");
+        System.out.println("Nationality: Indian");
+        System.out.println("E-mail: pratikpatidar5350@gmail.com");
+        System.out.println("");
+        System.out.println("Address :- Dehariya Sahu Hatpipliya Dewas MP (455223) ");
+
+
+    }
+
+    public static void education(){
+        System.out.println("***************************EDUCATIONAL QUALIFICATION***************************");
+        System.out.println("");
+        System.out.println("BARKTULLAH UNIVERSITY BHOPAL");
+        System.out.println("B.E");
+        System.out.println("2020-Present");
+        System.out.println("");
+        System.out.println("School :- Rukhma Devi Higher Secondary School Dudhiya Indore Mp ");
+        System.out.println("Class 12");
+        System.out.println("2018-20");
+        System.out.println("");
+        System.out.println("School :- Rukhma Devi Higher Secondary School Dudhiya Indore Mp ");
+        System.out.println("Class 10");
+        System.out.println("2008-18");
+        System.out.println("");
+        System.out.println("");
+
+
+    }
+    public static void skills(){
+        System.out.println("******************************** SKILLS KNOWN ********************************");
+
+        System.out.println("");
+        System.out.println("CSS");
+        System.out.println("C");
+        System.out.println("C++ Programming Language");
+        System.out.println("Java Programming Language");
+        System.out.println("HTML");
+
+
+        System.out.println("");
+        System.out.println("");
+    }
+
+    public static void projects(){
+        System.out.println("******************************** PROJECT ******************************** ");
+
+        System.out.println("");
+        System.out.println(" Library Management system :Using Java Programming");
+        System.out.println("");
+    }
+    public static void signature(){
+        System.out.println("******************************** DECLARATION ******************************** ");
+
+        System.out.println("");
+        System.out.println("I do hereby declare that the information given in this document are true ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("SIGNATURE: ");
+        System.out.println("");
+    }
+    public static void main(String args[]){
+
+        about();
+        education();
+        skills();
+        projects();
+        signature();
+    }
+}
